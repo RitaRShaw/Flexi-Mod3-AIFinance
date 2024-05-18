dec04894d1647e8a1f3232a0085ff346e045ecd9
RitaRShaw <rita_shaw@yahoo.com>
2024-05-18 14:41:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,65 @@
 from flask import Flask, render_template,request
+import google.generativeai as palm
+import replicate
+import os
 
-app=Flask(__name__)
+flag = 1
+name = ""
 
-@app.route("/",methods=["GET","POST"])
+palm.configure(api_key="AIzaSyAv9fecdj0fnj1KR6awiYSkaqpZWTSSINI")
+os.environ["REPLICATE_API_TOKEN"] = "r8_1R8QK9KyBP7gBYJPQgo7UjBXz1A44I62W3YTE"
+
+model = {"model" : "models/chat-bison-001"}
+
+app = Flask(__name__)
+
+@app.route("/",methods = ["GET","POST"])
 def index():
     return(render_template("index.html"))
 
-@app.route("/main",methods=["GET","POST"])
+@app.route("/main",methods = ["GET","POST"])
 def main():
-    r=request.form.get("q")
-    return(render_template("main.html",r=r))
+    global flag, name
+    if flag == 1:
+        name = request.form.get("q")
+        flag = 0
+    return(render_template("main.html",r=name))
 
-@app.route("/prediction",methods=["GET","POST"])
+@app.route("/prediction",methods = ["GET","POST"])
 def prediction():
     return(render_template("prediction.html"))
 
-@app.route("/dbs_price",methods=["GET","POST"])
+@app.route("/dbs_price",methods = ["GET","POST"])
 def dbs_price():
-    q=float(request.form.get("q"))
+    q = float(request.form.get("q"))
     return(render_template("dbs_price.html",r=(q*-50.6)+90.2))
 
-if __name__=="__main__":
+@app.route("/generate_text",methods = ["GET","POST"])
+def generate_text():
+    return(render_template("generate_text.html"))
+
+@app.route("/text_result_makersuite",methods = ["GET","POST"])
+def text_result_makersuite():
+    q = request.form.get("q")
+    r = palm.chat(**model, messages=q)
+    return(render_template("text_result_makersuite.html",r=r.last))
+    
+@app.route("/generate_image",methods = ["GET","POST"])
+def generate_text():
+    return(render_template("generate_image.html"))
+
+@app.route("/image-result",methods = ["GET","POST"])
+def image_result():
+    q = request.form.get("q")
+    r=replicate.run("stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf", input={"prompt":q})
+    return(render_template("image_result.html",r=r[0]))
+
+
+@app.route("/end",methods = ["GET","POST"])
+def end():
+    global flag
+    flag = 1
+    return(render_template("index.html"))
+
+if __name__ == "__main__":
     app.run()
